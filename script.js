a595ba228ba1e42bee1c6f73cc95fe1652eaf93a
Espresso38 <naj.tomalak@gmail.com>
2024-07-25 18:04:21 +0200

    Creating script for calcuator

--- script.js
+++ script.js
@@ -5,3 +5,118 @@
 4 funkcje po naciśnięciu
 5 egde casy
 */
+class Calculator {
+    constructor(prevOperandText, currOperandText) {
+        this.prevOperandText = prevOperandText
+        this.currOperandText = currOperandText
+        this.clear()
+    }
+
+    clear() {
+        this.prevOperand = ""
+        this.currOperand = ""
+        this.operation = undefined
+    }
+
+    appendNum(number) {
+        if (number === '.' && this.currOperand.includes('.')) return
+        this.currOperand = this.currOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+        if (this.currOperand === '') return
+        if (this.prevOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.prevOperand = this.currOperand
+        this.currOperand = ""
+    }
+
+    compute() {
+        let computation
+        const prev = parseFloat(this.prevOperand)
+        const curr = parseFloat(this.currOperand)
+        if (isNaN(prev) || isNaN(curr)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + curr
+                break
+            case '-':
+                computation = prev - curr
+                break
+            case '*':
+                computation = prev * curr
+                break
+            case '÷':
+                if (curr === 0) return computation = 'Zero division Error'
+                computation = prev / curr
+                break
+            default:
+                return
+        }
+        this.currOperand = computation
+        this.operation = undefined
+        this.prevOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const strignNum = number.toString()
+        const intDigits = parseFloat(strignNum.split('.')[0])
+        const decimalDigits = strignNum.split('.')[1]
+        let intDisplay
+        if (isNaN(intDigits)) {
+            intDisplay = ''
+        } else {
+            intDisplay = intDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${intDisplay}.${decimalDigits}`
+        } else {
+            return intDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currOperandText.innerText = this.getDisplayNumber(this.currOperand)
+        if (this.operation != null) {
+            this.prevOperandText.innerText = `${this.getDisplayNumber(this.prevOperand)} ${this.operation}`
+        } else {
+            this.prevOperandText.innerText = ''
+        }
+    }
+}
+
+
+const numButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation')
+const equalsButton = document.querySelector('[data-equals]')
+const clearButton = document.querySelector('[data-clear]')
+const prevOperandText = document.querySelector('[data-prev-operand]')
+const currOperandText = document.querySelector('[data-curr-operand]')
+
+const calculator = new Calculator(prevOperandText, currOperandText)
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNum(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+clearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
